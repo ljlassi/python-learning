ae18d4230b44d383f096094a0bcd75cff2601893
Lassi Jääskeläinen <lassi@lassijaaskelainen.fi>
2020-06-23 14:48:00 +0300

    Changed car manufacturer to Ford

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 
 from car import Car
 
-my_volvo = Car("Volvo", "pickup", 300, 4000)
+my_volvo = Car("Ford", "pickup", 300, 4000)
 car_evaluation = my_volvo.evaluateCar()
 for x in range(len(car_evaluation)):
     print(car_evaluation[x])
